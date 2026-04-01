8b16505a0312f71c31fdee2f7eee42ee32a2d48a
mainamuchiru <mainaephantus@gmail.com>
2026-04-01 12:50:48 +0300

    Code updated

--- index.js
+++ index.js
@@ -1,6 +1,21 @@
+function createLoginTracker(userInfo) {
+  let attemptCount = 0;
 
+  return (passwordAttempt) => {
+    attemptCount++;   
 
+    if (passwordAttempt === userInfo.password && attemptCount <= 3) {
+      return "Login successful";
+    } 
+    else if (attemptCount <= 3) {
+      return `Attempt ${attemptCount}: Login failed`;
+    } 
+    else {
+      return "Account locked due to too many failed login attempts";
+    }
+  };
+}
 
 module.exports = {
-  ...(typeof createLoginTracker !== 'undefined' && { createLoginTracker })
+  ...(typeof createLoginTracker !== "undefined" && { createLoginTracker }),
 };
